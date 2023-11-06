55ece8aabe90a435e1373f8f5f6cd9f7798df84d
HeddaIH <148773536+HeddaIH@users.noreply.github.com>
2023-11-06 15:10:55 +0100

    Update BME680.ino
    LOL, potet hei 👍

--- BME680/BME680.ino
+++ BME680/BME680.ino
@@ -3,20 +3,25 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME680.h>
-
+int lyd = A0;
+int lys = A1;
 Adafruit_BME680 bme; // create BME680 object
 
 void setup() {
+    pinMode(lyd,INPUT);
+    pinMode(lys, INPUT)
     Serial.begin(9600);
     while (!Serial); // wait for serial port to connect
     if (!bme.begin(0x76)) {
         Serial.println(F("Could not find a valid BME680 sensor, check wiring!!"));
         while (1);
     }
-    Serial.println("Temb,Pressure, humidity, gas");
+    Serial.println("Temb,Pressure, humidity, gas, lyd, lys");
 }
 
 void loop() {
+    int lydniva = analogRead(lyd);
+    int lysniva = analogRead(lys)
     if (! bme.performReading()) {
         Serial.println("Failed to perform reading :(");
         return;
@@ -29,6 +34,10 @@ void loop() {
     Serial.print(bme.humidity);
     Serial.print(",");
     Serial.print(bme.gas_resistance / 1000.0);
+    Serial.print(",");
+    Serial.print(lydniva);
+    Serial.print(",");
+    Serial.print(lysniva);
     Serial.println();
 
     delay(1000);
