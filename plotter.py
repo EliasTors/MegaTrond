19944deae132a9809b87d627bfe10a258857dd4b
Vorckea <akselschrader@gmail.com>
2023-11-23 15:09:10 +0100

    Plotter

--- plotter.py
+++ plotter.py
@@ -1,44 +1,45 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import datetime
 
-# Read the CSV file into a pandas dataframe
-df = pd.read_csv('test.csv')
+class plotter:
+    def plot_data(self, filename, interval):
+        data = pd.read_csv(filename)
 
-# Extract the data columns
-datetime = df['datetime']
-temp = df['temp']
-pressure = df['pressure']
-humidity = df['humidity']
-gas = df['gas']
-lyd = df['lyd']
-lys = df['lys']
+        data['datetime'] = pd.to_datetime(data['datetime'])
 
-# Plot the data
-plt.figure(figsize=(8, 6))
+        data['date'] = data['datetime'].dt.date
+        data['time'] = data['datetime'].apply(lambda x: datetime.datetime.combine(datetime.date.today(), x.time()))
 
-plt.subplot(3, 2, 1)
-plt.plot(datetime, temp, label='Temperature', alpha=0.7)
-plt.legend()
+        room_codes = {datetime.date(2023, 11, 16): 'R11', 
+                    datetime.date(2023, 11, 17): 'R22', 
+                    datetime.date(2023, 11, 18): 'R04', 
+                    datetime.date(2023, 11, 19): 'FYS'}
+        data['room'] = data['date'].map(room_codes)
 
-plt.subplot(3, 2, 2)
-plt.plot(datetime, pressure, label='Pressure', alpha=0.7)
-plt.legend()
+        fig, axs = plt.subplots(6, figsize=(10, 20))
 
-plt.subplot(3, 2, 3)
-plt.plot(datetime, humidity, label='Humidity', alpha=0.7)
-plt.legend()
+        rooms = data['room'].unique()
 
-plt.subplot(3, 2, 4)
-plt.plot(datetime, gas, label='Gas')
-plt.legend()
+        for room in rooms:
+            data_room = data[data['room'] == room]
+            data_room = data_room.set_index('time').resample(interval).mean().reset_index()
+            axs[0].plot(data_room['time'], data_room['temp'], label=f'Temperature in {room}')
+            axs[1].plot(data_room['time'], data_room['pressure'], label=f'Pressure in {room}')
+            axs[2].plot(data_room['time'], data_room['humidity'], label=f'Humidity in {room}')
+            axs[3].plot(data_room['time'], data_room['gas'], label=f'Gas in {room}')
+            axs[4].plot(data_room['time'], data_room['lyd'], label=f'Lyd in {room}')
+            axs[5].plot(data_room['time'], data_room['lys'], label=f'Lys in {room}')
 
-plt.subplot(3, 2, 5)
-plt.plot(datetime, lyd, label='Sound')
-plt.legend()
+        for i, label in enumerate(['Temperature (°C)', 'Pressure (hPa)', 'Humidity (%)', 'Gas', 'Lyd', 'Lys']):
+            axs[i].set_xlabel('Time')
+            axs[i].set_ylabel(label)
+            axs[i].set_title(f'{label} over Time')
+            axs[i].xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+            axs[i].legend()
 
-plt.subplot(3, 2, 6)
-plt.plot(datetime, lys, label='Light')
-plt.legend()
+        plt.tight_layout()
+        plt.show()
 
-plt.subplots_adjust(hspace=0.5, wspace=0.3)
-plt.show()
+
