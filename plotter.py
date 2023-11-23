baeaaad6e3eeee8ad3dd1353ccb069ad9cf1b5ec
EliasTors <46528691+EliasTors@users.noreply.github.com>
2023-11-23 09:49:15 +0100

    Update plotter

--- plotter.py
+++ plotter.py
@@ -1,44 +1,38 @@
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Read the CSV file into a pandas dataframe
-df = pd.read_csv('test.csv')
+# Read the CSV file into a pandas dataframe with optimized parameters
+df = pd.read_csv('test.csv', parse_dates=['datetime'], dtype={'temp': float, 'pressure': float, 'humidity': float, 'gas': float, 'lyd': float, 'lys': float})
 
-# Extract the data columns
-datetime = df['datetime']
-temp = df['temp']
-pressure = df['pressure']
-humidity = df['humidity']
-gas = df['gas']
-lyd = df['lyd']
-lys = df['lys']
+# Plot the data using Seaborn
+sns.set(style="darkgrid", palette="muted")
 
-# Plot the data
 plt.figure(figsize=(8, 6))
 
 plt.subplot(3, 2, 1)
-plt.plot(datetime, temp, label='Temperature', alpha=0.7)
-plt.legend()
+sns.lineplot(x='datetime', y='temp', data=df)
+plt.title('Temperature')
 
 plt.subplot(3, 2, 2)
-plt.plot(datetime, pressure, label='Pressure', alpha=0.7)
-plt.legend()
+sns.lineplot(x='datetime', y='pressure', data=df)
+plt.title('Pressure')
 
 plt.subplot(3, 2, 3)
-plt.plot(datetime, humidity, label='Humidity', alpha=0.7)
-plt.legend()
+sns.lineplot(x='datetime', y='humidity', data=df)
+plt.title('Humidity')
 
 plt.subplot(3, 2, 4)
-plt.plot(datetime, gas, label='Gas')
-plt.legend()
+sns.lineplot(x='datetime', y='gas', data=df)
+plt.title('Gas')
 
 plt.subplot(3, 2, 5)
-plt.plot(datetime, lyd, label='Sound')
-plt.legend()
+sns.lineplot(x='datetime', y='lyd', data=df)
+plt.title('Sound')
 
 plt.subplot(3, 2, 6)
-plt.plot(datetime, lys, label='Light')
-plt.legend()
+sns.lineplot(x='datetime', y='lys', data=df)
+plt.title('Light')
 
 plt.subplots_adjust(hspace=0.5, wspace=0.3)
 plt.show()
