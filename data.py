ec5d383e56d95cc6a1ca2da8e9df2de11b33a577
Vorckea <akselschrader@gmail.com>
2023-11-25 23:48:14 +0100

    small fixes

--- data.py
+++ data.py
@@ -15,64 +15,60 @@ class dataHandler:
             file.write(content)
     
     def sql_to_csv(db_file, csv_file="data.csv", table="data"):
-        con = sqlite3.connect(db_file)
-        query = f"SELECT datetime, line FROM {table}"
-        df = pd.read_sql_query(query, con)
-        df.to_csv(csv_file, index=False)
-        con.close()
+        # Connect to the database and fetch data
+        with sqlite3.connect(db_file) as con:
+            query = f"SELECT datetime, line FROM {table}"
+            df = pd.read_sql_query(query, con)
 
-        # Open the CSV file and replace all "/n/n" with "/n"
+        # Write data to CSV
+        df.to_csv(csv_file, index=False)
+
+        # Read the CSV file
         with open(csv_file, 'r') as file:
             content = file.readlines()
 
-        # Modify the first line to "datetime,temp,pressure,humidity,gas,lyd,lys"
+        # Modify the first line
         content[0] = "datetime,temp,pressure,humidity,gas,lyd,lys\n"
 
-        # Remove lines containing "error" or "Temp,Pressure, humidity, gas, lyd, lys"
-        content = [line for line in content if "error" not in line and "Temp,Pressure, humidity, gas, lyd, lys" not in line]
+        # Filter and modify content
+        content = [
+            line.replace('\n\n', '\n').replace('"', '')
+            for line in content
+            if "error" not in line
+            and "Temp,Pressure, humidity, gas, lyd, lys" not in line
+            and len(line.split(',')) == 7
+        ]
 
-        # Remove lines that are not complete (do not contain all the expected columns)
-        content = [line for line in content if len(line.split(',')) == 7]
-
-        # Replace "\n\n" with "\n" and '""' with ''
-        content = [line.replace('\n\n', '\n').replace('"', '') for line in content]
-
+        # Write the modified content back to the CSV file
         with open(csv_file, 'w') as file:
             file.writelines(content)
-
-class dataFixer:
+            
     def shift_datetime(sqlite_file, start_time, table_name="data"):
         # Convert the start_time string to a datetime object
         start_datetime = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%S")
 
-        # Connect to the SQLite database
-        conn = sqlite3.connect(sqlite_file)
-        cursor = conn.cursor()
+        # Connect to the SQLite database and fetch all rows from the table
+        with sqlite3.connect(sqlite_file) as conn:
+            cursor = conn.cursor()
+            cursor.execute(f"SELECT * FROM {table_name}")
+            rows = cursor.fetchall()
 
-        # Fetch all rows from the table
-        cursor.execute(f"SELECT * FROM {table_name}")
-        rows = cursor.fetchall()
+            # Get the first datetime value in the table
+            first_datetime = datetime.strptime(rows[0][0], "%Y-%m-%dT%H:%M:%S.%f")
 
-        # Get the first datetime value in the table
-        first_datetime = datetime.strptime(rows[0][0], "%Y-%m-%dT%H:%M:%S.%f")
-        
-        # Iterate through the rows and update the datetime values
-        for row in rows:
-            time_diff = datetime.strptime(row[0], "%Y-%m-%dT%H:%M:%S.%f") - first_datetime
-            new_datetime = start_datetime + time_diff
-            new_datetime_str = new_datetime.strftime("%Y-%m-%dT%H:%M:%S.%f")
-            
+            # Iterate through the rows and update the datetime values
+            for row in rows:
+                time_diff = datetime.strptime(row[0], "%Y-%m-%dT%H:%M:%S.%f") - first_datetime
+                new_datetime = start_datetime + time_diff
+                new_datetime_str = new_datetime.strftime("%Y-%m-%dT%H:%M:%S.%f")
 
-            # Update the row with the new datetime value
-            cursor.execute(f"UPDATE {table_name} SET datetime = ? WHERE line = ?", (new_datetime_str, row[1]))
+                # Update the row with the new datetime value
+                cursor.execute(f"UPDATE {table_name} SET datetime = ? WHERE line = ?", (new_datetime_str, row[1]))
 
-        # Commit the changes and close the connection
-        conn.commit()
-        conn.close()
+            # Commit the changes
+            conn.commit()
 
-        
-        
-        
+
         '''
         # Connect to the SQLite database
         conn = sqlite3.connect(filepath)
